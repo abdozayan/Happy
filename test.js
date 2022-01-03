37bd05b5b04561f04806375c3af306d822d75e78
abdozayan <96956593+abdozayan@users.noreply.github.com>
2022-01-03 07:52:52 +0200

    Add files via upload

---
+++ test.js
@@ -0,0 +1,6 @@
+function test() {
+    var name1 = "Happy new year, How are you?, I love you.";
+    if(name1 ==="Happy new year, How are you?, I love you."){
+        alert("hello"+ name1);
+    }
+    }
